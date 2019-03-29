6585b2fb0b48d0162a878a2350ee3533e1a863af
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-29 16:20:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "guy"
+
+let welcomeMessage = `Welcome to Flatbook, guy!`
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, GUY!"
+
+let shortGreeting = "Welcome, g!"
